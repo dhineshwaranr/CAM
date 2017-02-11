546d04cdce8f7a7247b5615eecca325ef0957067
dhineshwaranr <dhinesh.kr13@gmail.com>
2017-02-12 02:47:20 +0530

    First Initial Project Commit

---
+++ CAM/src/main/java/com/cam/CamApplication.java
@@ -0,0 +1,12 @@
+package com.cam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamApplication.class, args);
+	}
+}
